b1f2ddc0317d33e5e781f43ef0f43dd6447eef82
Georg Dangl <georg@georgdangl.de>
2016-06-25 01:09:13 +0200

    Add angular components, templates and styles

---
+++ src/NetCoreHeroes/wwwroot/app/app.component.ts
@@ -0,0 +1,52 @@
+﻿/*
+ * See here if using Typescript 1.8.31 extensions, there's a bug that needs some manual fixing:
+ * https://github.com/Microsoft/TypeScript/issues/8518
+ */
+
+import { Component }       from '@angular/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@Component({
+    selector: 'my-app',
+    template: `
+  <h1>{{title}}</h1>
+  <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
+  `,
+    directives: [
+        ROUTER_DIRECTIVES
+    ],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+    ],
+    styleUrls: ['/app/app.component.css']
+})
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+        },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    }
+])
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
---
+++ src/NetCoreHeroes/wwwroot/app/hero.ts
@@ -0,0 +1,4 @@
+﻿export class Hero {
+    id: number;
+    name: string;
+}
