32fb640b2134c6a08e2856108e9f981a029866e4
Georg Dangl <georg@georgdangl.de>
2016-06-24 21:00:28 +0200

    Add empty Home/Index View

---
+++ src/NetCoreHeroes/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreHeroes.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [Route("/{*angularUrl}")]
+        public IActionResult Index(string angularUrl)
+        {
+            ViewData["AngularBase"] = Url.Action(nameof(Index), new { angularUrl = string.Empty });
+            return View();
+        }
+    }
+}
