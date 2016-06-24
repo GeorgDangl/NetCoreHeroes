936bc87d073ea61ad5ccbbf5cfa3ba623f55b26b
Georg Dangl <georg@georgdangl.de>
2016-06-25 00:55:19 +0200

    Replace node_module with lib in systemjs.config.js paths and remove in-memory-web-api maps

--- src/NetCoreHeroes/wwwroot/systemjs.config.js
+++ src/NetCoreHeroes/wwwroot/systemjs.config.js
@@ -6,15 +6,13 @@
     // map tells the System loader where to look for things
     var map = {
         'app': 'app', // 'dist',
-        '@angular': 'node_modules/@angular',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        'rxjs': 'node_modules/rxjs'
+        '@angular': 'lib/@angular',
+        'rxjs': 'lib/rxjs'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': { main: 'main.js', defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' },
-        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
     };
     var ngPackageNames = [
       'common',
