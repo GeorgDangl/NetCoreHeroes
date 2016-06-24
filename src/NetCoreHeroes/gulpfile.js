c1101e2193030a152400ba01441c1bc556417645
Georg Dangl <georg@georgdangl.de>
2016-06-24 22:54:03 +0200

    Add gulp; Add gulpfile with task to copy client dependencies to wwwroot; Include this task in npm postinstall

---
+++ src/NetCoreHeroes/gulpfile.js
@@ -0,0 +1,23 @@
+﻿var gulp = require('gulp');
+
+var paths = {
+    nodeModules: "./node_modules/",
+    clientDeps: "./wwwroot/lib/"
+};
+
+var clientLibraries = [
+    "core-js",
+    "zone-js",
+    "reflect-metadata",
+    "systemjs",
+    "@angular",
+    "rxjs"
+];
+
+gulp.task("copyClientDeps",
+    function() {
+        for (var i = 0; i < clientLibraries.length; i++) {
+            gulp.src(paths.nodeModules + clientLibraries[i] + "/**/*")
+                .pipe(gulp.dest(paths.clientDeps + clientLibraries[i]));
+        }
+    });
