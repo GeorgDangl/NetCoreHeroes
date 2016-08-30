7d5bc44e913e37b1d4e914730cc4cde3d7b5a1f3
Georg Dangl <georg@georgdangl.de>
2016-08-30 23:11:28 +0200

    Remove unnecessary JsonPropertyAttributes in Hero.cs
    Since .Net Core 1.0.0, API responses are automatically transformed to camelCase

--- src/NetCoreHeroes/Data/Hero.cs
+++ src/NetCoreHeroes/Data/Hero.cs
@@ -4,15 +4,12 @@ namespace NetCoreHeroes.Data
 {
     public class Hero
     {
-        [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
-        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
     }
 
     public class HeroPost
     {
-        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
     }
 }
