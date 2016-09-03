8409674baa9ed02e144375061a755da0949cc14c
Georg Dangl <georg@georgdangl.de>
2016-09-03 20:51:46 +0200

    Remove testsetup (obsolete now since Angular 2 RC 6)

--- src/NetCoreHeroes/wwwroot/testSetup.js
+++
@@ -1,3 +0,0 @@
-﻿// MockBackend does have trouble with umd packages, see https://github.com/angular/angular/issues/9170#issuecomment-229947428 for more info
-// Therefore, systemjs is set to packageWithIndex when running unit tests
-System.packageWithIndex = true;
