f35c14d9246adcab1a955b2c992f4c84760d162d
Georg Dangl <georg@georgdangl.de>
2016-09-03 15:19:45 +0200

    Add gulp task to delete wwwroot/lib client dependencies

--- src/NetCoreHeroes/gulpfile.js
+++ src/NetCoreHeroes/gulpfile.js
@@ -1,27 +1,33 @@
 ﻿var gulp = require('gulp');
 var merge = require('merge-stream');
+var rimraf = require('rimraf');
 
 var paths = {
-    nodeModules: "./node_modules/",
-    clientDeps: "./wwwroot/lib/"
+    nodeModules: './node_modules/',
+    clientDeps: './wwwroot/lib/'
 };
 
 var clientLibraries = [
-    "core-js",
-    "zone.js",
-    "reflect-metadata",
-    "systemjs",
-    "@angular",
-    "rxjs",
-    "es6-shim"
+    'core-js',
+    'zone.js',
+    'reflect-metadata',
+    'systemjs',
+    '@angular',
+    'rxjs',
+    'es6-shim'
 ];
 
-gulp.task("copyClientDeps",
+gulp.task('copyClientDeps',
     function () {
         var mergeStream = merge();
         for (var i = 0; i < clientLibraries.length; i++) {
-            mergeStream.add(gulp.src([paths.nodeModules + clientLibraries[i] + "/**/*", '!' + paths.nodeModules + clientLibraries[i] + "/**/*tsconfig.json"])
+            mergeStream.add(gulp.src([paths.nodeModules + clientLibraries[i] + '/**/*', '!' + paths.nodeModules + clientLibraries[i] + '/**/*tsconfig.json'])
                 .pipe(gulp.dest(paths.clientDeps + clientLibraries[i])));
         }
         return mergeStream;
+    });
+
+gulp.task('cleanClientDeps',
+    function(cb) {
+        return rimraf(paths.clientDeps, cb);
     });
