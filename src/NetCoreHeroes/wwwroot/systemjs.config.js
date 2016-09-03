4e9450fc3b62d04c8e7d0f3cfcd5efbd8ea4ed1c
Georg Dangl <georg@georgdangl.de>
2016-09-03 20:50:13 +0200

    Update systemjs.config.js for Angular 2 RC 6

--- src/NetCoreHeroes/wwwroot/systemjs.config.js
+++ src/NetCoreHeroes/wwwroot/systemjs.config.js
@@ -3,44 +3,88 @@
  * Adjust as necessary for your application needs.
  */
 (function (global) {
-    // map tells the System loader where to look for things
-    var map = {
-        'app': 'app', // 'dist',
-        '@angular': 'lib/@angular',
-        'rxjs': 'lib/rxjs'
-    };
-    // packages tells the System loader how to load when no filename and/or no extension
-    var packages = {
-        'app': { main: 'main.js', defaultExtension: 'js' },
-        'rxjs': { defaultExtension: 'js' },
-    };
-    var ngPackageNames = [
-      'common',
-      'compiler',
-      'core',
-      'forms',
-      'http',
-      'platform-browser',
-      'platform-browser-dynamic',
-      'router',
-      'router-deprecated',
-      'upgrade',
-    ];
-    // Individual files (~300 requests):
-    function packIndex(pkgName) {
-        packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    }
-    // Bundled (~40 requests):
-    function packUmd(pkgName) {
-        packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-    }
-    // Most environments should use UMD; some (Karma) need the individual index files
-    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-    // Add package entries for angular packages
-    ngPackageNames.forEach(setPackageConfig);
-    var config = {
-        map: map,
-        packages: packages
-    };
-    System.config(config);
+    //// map tells the System loader where to look for things
+    //var map = {
+    //    'app': 'app', // 'dist',
+    //    '@angular': 'lib/@angular',
+    //    'rxjs': 'lib/rxjs'
+    //};
+    //// packages tells the System loader how to load when no filename and/or no extension
+    //var packages = {
+    //    'app': { main: 'main.js', defaultExtension: 'js' },
+    //    'rxjs': { defaultExtension: 'js' },
+    //};
+    //var ngPackageNames = [
+    //  'common',
+    //  'compiler',
+    //  'core',
+    //  'core/testing',
+    //  'forms',
+    //  'http',
+    //  'http/testing',
+    //  'platform-browser',
+    //  'platform-browser-dynamic',
+    //  'platform-browser-dynamic/testing',
+    //  'router',
+    //  'upgrade'
+    //];
+    //// Individual files (~300 requests):
+    //function packIndex(pkgName) {
+    //    packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    //}
+    //// Bundled (~40 requests):
+    //function packUmd(pkgName) {
+    //    packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+    //}
+    //// Most environments should use UMD; some (Karma) need the individual index files
+    //var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    //// Add package entries for angular packages
+    //ngPackageNames.forEach(setPackageConfig);
+    //var config = {
+    //    map: map,
+    //    packages: packages
+    //};
+    //System.config(config);
+
+    System.config({
+        paths: {
+            // paths serve as alias
+            'lib:': 'lib/'
+        },
+        // map tells the System loader where to look for things
+        map: {
+            // our app is within the app folder
+            app: 'app',
+            // angular bundles
+            '@angular/core': 'lib:@angular/core/bundles/core.umd.js',
+            '@angular/core/testing': 'lib:@angular/core/bundles/core-testing.umd.js',
+            '@angular/common': 'lib:@angular/common/bundles/common.umd.js',
+            '@angular/compiler': 'lib:@angular/compiler/bundles/compiler.umd.js',
+            '@angular/compiler/testing': 'lib:@angular/compiler/bundles/compiler-testing.umd.js',
+            '@angular/platform-browser': 'lib:@angular/platform-browser/bundles/platform-browser.umd.js',
+            '@angular/platform-browser/testing': 'lib:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+            '@angular/platform-browser-dynamic': 'lib:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/platform-browser-dynamic/testing': 'lib:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+            '@angular/http': 'lib:@angular/http/bundles/http.umd.js',
+            '@angular/http/testing': 'lib:@angular/http/bundles/http-testing.umd.js',
+            '@angular/router': 'lib:@angular/router/bundles/router.umd.js',
+            '@angular/forms': 'lib:@angular/forms/bundles/forms.umd.js',
+            // other libraries
+            'rxjs': 'lib:rxjs'
+        },
+        // packages tells the System loader how to load when no filename and/or no extension
+        packages: {
+            app: {
+                main: './main.js',
+                defaultExtension: 'js'
+            },
+            rxjs: {
+                defaultExtension: 'js'
+            },
+            'angular2-in-memory-web-api': {
+                main: './index.js',
+                defaultExtension: 'js'
+            }
+        }
+    });
 })(this);
