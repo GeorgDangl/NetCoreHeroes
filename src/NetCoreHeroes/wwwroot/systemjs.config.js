dee2ddf2be4953abb1899f72cb7dd37136bdebec
Georg Dangl <georg@georgdangl.de>
2016-09-04 22:19:09 +0200

    Remove obsolete entry for SystemJS

--- src/NetCoreHeroes/wwwroot/systemjs.config.js
+++ src/NetCoreHeroes/wwwroot/systemjs.config.js
@@ -37,10 +37,6 @@
             },
             rxjs: {
                 defaultExtension: 'js'
-            },
-            'angular2-in-memory-web-api': {
-                main: './index.js',
-                defaultExtension: 'js'
             }
         }
     });
