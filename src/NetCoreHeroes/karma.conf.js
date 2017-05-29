f4d130ef961edac6204fa58cdd1a0c6ff6eb14f2
Georg Dangl <georg@dangl.me>
2017-05-29 22:34:17 +0200

    Add Karma support to run TypeScript / JavaScript tests on Linux without Chutzpah

---
+++ src/NetCoreHeroes/karma.conf.js
@@ -0,0 +1,32 @@
+﻿module.exports = function (config) {
+    config.set({
+        frameworks: ['jasmine', 'karma-typescript'],
+        files: [
+            'node_modules/es6-shim/es6-shim.js',
+            'node_modules/core-js/client/shim.min.js',
+            'node_modules/reflect-metadata/Reflect.js',
+            'node_modules/systemjs/dist/system.src.js',
+            'node_modules/zone.js/dist/zone.js',
+            'node_modules/zone.js/dist/async-test.js',
+            'node_modules/zone.js/dist/fake-async-test.js',
+            'node_modules/zone.js/dist/sync-test.js',
+            'node_modules/zone.js/dist/proxy.js',
+            'node_modules/zone.js/dist/jasmine-patch.js',
+            'systemjs.chutzpah.config.js',
+            'App/hero.ts',
+            'App/**/*.service.ts',
+            'App/**/*.spec.ts'
+        ],
+        preprocessors: {
+            'App/**/*.ts': [ 'karma-typescript']
+        },
+        reporters: ['progress', 'dots', 'karma-typescript', 'junit'],
+        browsers: ['PhantomJS'],
+        singleRun: true,
+        junitReporter: {
+            outputDir: '',
+            outputFile: 'karma-results.xml',
+            useBrowserName: false
+        }
+    });
+}
